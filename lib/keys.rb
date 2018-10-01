2cb4fa01718d91d5a39c8569258eb0888bb97d85
Merline Belizaire <merline@noukod.com>
2018-10-01 19:53:42 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -10,5 +10,5 @@ require 'pry'
 
 def get_the_keys(groceries)
   #code your solution here!
-  arr = groceries[:dairy] [:vegetable] [:meat] [:grains]
+ groceries[:dairy] [:vegetable] [:meat] [:grains]
 end
